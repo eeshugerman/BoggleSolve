ebcf431a1a36de319546501c3990e50413510f1c
Elliott Shugerman <eeshugerman@gmail.com>
2021-02-15 14:55:08 -0500

    indent public/private blocks

--- boggle.h
+++ boggle.h
@@ -12,26 +12,26 @@ struct Tile {
 };
 
 class Boggle {
-public:
-  Boggle(Dictionary *dict);
-  ~Boggle();
-  void fillBoardRandom();
-  void fillBoardUser(std::vector<char> letters);
-  void printBoard();
-  void findWords(Tile *prev, int i, int j);
-  void printWords();
-  void saveWords();
-  void clearWords();
+  public:
+    Boggle(Dictionary *dict);
+    ~Boggle();
+    void fillBoardRandom();
+    void fillBoardUser(std::vector<char> letters);
+    void printBoard();
+    void findWords(Tile *prev, int i, int j);
+    void printWords();
+    void saveWords();
+    void clearWords();
 
-private:
-  std::string buildWord(Tile *prev_path);
-  bool checkVisited(Tile *to_check, Tile *current);
-  void removeDuplicateWords();
+  private:
+    std::string buildWord(Tile *prev_path);
+    bool checkVisited(Tile *to_check, Tile *current);
+    void removeDuplicateWords();
 
-  Dictionary *dictionary;
-  Tile *board[4][4];
-  std::vector<std::string> words;
-  std::vector<Tile *> search_tiles;
+    Dictionary *dictionary;
+    Tile *board[4][4];
+    std::vector<std::string> words;
+    std::vector<Tile *> search_tiles;
 };
 
 #endif // GAME_H
--- dictionary.h
+++ dictionary.h
@@ -3,36 +3,26 @@
 #include <iostream>
 #include <string>
 
-class Dictionary // as a trie, aka prefix tree
-{
-
-private:
-  class Node {
+class Node {
   public:
     Node();
     ~Node();
     Node *parent;
     Node *children[26];
     bool is_word;
-    // Node(const Node &obj);
-    //{
-    // std::cout<<"copy constructor called" << std::endl;
-    //};
+};
 
-    // Node& operator=(const Node& obj)
-    //{
-    // std::cout<<"copy assignment operator called" << std::endl;
-    //};
-  };
-  int charToInt(char c);
-  Node *root;
+class Dictionary { // as a trie, aka prefix tree
+  private:
+    int charToInt(char c);
+    Node *root;
 
-public:
-  Dictionary();
-  ~Dictionary();
-  void addWord(std::string word);
-  bool isWord(std::string word);
-  bool isPrefix(std::string word);
+  public:
+    Dictionary();
+    ~Dictionary();
+    void addWord(std::string word);
+    bool isWord(std::string word);
+    bool isPrefix(std::string word);
 };
 
 #endif // PREFIXDICTIONARY_H
